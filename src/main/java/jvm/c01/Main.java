fc3e7017e688b6285fc25828c2c4bfeb5a0cf1f1
Ramesh Ponnada <rameshponnada@gmail.com>
2020-01-21 15:17:34 +0530

    JVM Memory Optimization

---
+++ src/main/java/jvm/c01/Main.java
@@ -0,0 +1,13 @@
+package jvm.c01;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PrimeNumbers primeNumbers = new PrimeNumbers();
+		Integer max = Integer.parseInt(args[0]);
+		primeNumbers.generateNumbers(max);
+	}
+
+}
